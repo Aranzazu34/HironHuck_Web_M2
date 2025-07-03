5bebd0b28b4a3376e406ede51f86323470a3a6ea
Aranzazu34 <aranzazuprag@gmail.com>
2025-07-03 21:34:42 +0200

    Add files via upload

---
+++ Actividad_02072025/Inserts_autores_comics.sql
@@ -0,0 +1,8 @@
+INSERT INTO comics.autores (nombre,apellido,fecha_nacimiento,alta_registro,modificacion_registro) VALUES
+	 ('Masashi','Kishimoto','1974-11-08','2025-07-02 21:36:45',NULL),
+	 ('Eiichiro','Oda','1975-01-01','2025-07-02 21:37:50',NULL),
+	 ('Akira','Toriyama','1955-04-05','2025-07-02 21:38:00',NULL),
+	 ('Len','Wein','1948-06-12','2025-07-02 21:39:23',NULL),
+	 ('Stan','Lee','1922-12-28','2025-07-02 21:41:05',NULL),
+	 ('Don','Rico','1912-09-26','2025-07-02 21:42:28',NULL),
+	 ('Henrik','Galeen','1881-01-07','2025-07-02 21:44:02',NULL);
---
+++ Actividad_02072025/Inserts_personajes_comics.sql
@@ -0,0 +1,7 @@
+INSERT INTO comics.personajes_comics (nombre,especie,frase) VALUES
+	 ('Monkey D. Luffy','Humano','Voy a ser el rey de los piratas'),
+	 ('Naruto','Humano','¡Nunca me retracto de mi palabra, ese es mi camino ninja, mi nindo!'),
+	 ('Trunk','Híbrido saiyajin y humano','Sé que tú eres mi padre, pero ahora eres mi maestro.'),
+	 ('Wolwerine','Mutante','Soy el mejor en lo que hago, pero lo que hago no es muy agradable'),
+	 ('Viuda Negra','Biónica','He vivido muchas vidas... pero he terminado de huir de mi pasado'),
+	 ('Nosferatus','Vampiro','Ven a mí');
